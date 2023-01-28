215e5117ca1edb03f0eae211120770bd6408b4fb
David <davidtucu@hotmail.com>
2023-01-28 14:08:28 -0800

    hacer validar que un listado contenga un item

--- src/sum.test.js
+++ src/sum.test.js
@@ -55,3 +55,15 @@ test('object assignment', () => {
   test('but there is a "stop" in Christoph', () => {
     expect('Christoph').toMatch(/stop/);
   });
+  const shoppingList = [
+    'diapers',
+    'kleenex',
+    'trash bags',
+    'paper towels',
+    'milk',
+  ];
+  
+  test('the shopping list has milk on it', () => {
+    expect(shoppingList).toContain('milk');
+    expect(new Set(shoppingList)).toContain('milk');
+  });
