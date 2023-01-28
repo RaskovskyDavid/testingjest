3f1e3b5fe431b6c2aa9ebc42b76b0991e44a7567
David <davidtucu@hotmail.com>
2023-01-28 07:03:23 -0800

    hacer una suma con validador tobe

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import { createElement } from 'react';
 import App from './App';
 
+
+// it('renders without crashing', () => {
+//   const div = document-createElement('dev');
+//   ReactDOM.render(<App />, div);
+//   expect(div.innerHTML).toContain('Hi there!');
+//   ReactDOM.unmountComponentAtNode(div);
+// })
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
